52259f50aff8894b8049b42db6be4cdf03081d0e
jaimebacares@hotmail.com <jaimebacares@hotmail.com>
2021-09-14 18:43:09 -0500

    Se agregó carrusel a Sprint 1 y se agrego Html y Css Sprint 2

---
+++ Sprint 3/dev/appiniciall/calculadora/src/components/CalculadoraBasica.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+    <h1>Calculadora Básica</h1>
+    <input type="number" v-model="n1" />
+    <input type="number" v-model="n2" />
+    <button @click="suma()">Sumar</button>
+    <span>{{ resultado }}</span>
+</div>
+</template>
+
+<script>
+export default {
+    name: "CalculadoraBasica",
+    data: () => {
+        return {
+            n1:null, 
+            n2:null, 
+            resultado:"",
+        };
+    },
+    methods: {
+        suma() {
+        this.resultado = parseFloat(this.n1) + parseFloat(this.n2);
+        },
+    }, 
+};
+</script>
+
+<style>
+
+</style>
