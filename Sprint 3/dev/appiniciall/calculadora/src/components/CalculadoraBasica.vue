7df10a3a28c309c38826ca2d130af87198b986c0
jaimebacares@hotmail.com <jaimebacares@hotmail.com>
2021-09-15 16:30:17 -0500

    Páginas internas y cambios en el menú y otros

--- Sprint 3/dev/appiniciall/calculadora/src/components/CalculadoraBasica.vue
+++ Sprint 3/dev/appiniciall/calculadora/src/components/CalculadoraBasica.vue
@@ -1,9 +1,39 @@
 <template>
 <div>
     <h1>Calculadora Básica</h1>
-    <input type="number" v-model="n1" />
-    <input type="number" v-model="n2" />
-    <button @click="suma()">Sumar</button>
+    <v-text-field
+        type="number"
+        label="Number 1"
+        :rules="rules"
+        hide-details="auto"
+        v-model="n1"
+    ></v-text-field>   
+
+    <v-text-field
+        type="number"
+        label="Number 2"
+        :rules="rules"
+        hide-details="auto"
+        v-model="n2"
+    ></v-text-field>  
+
+    <v-btn
+        color="primary"
+        elevation="7"
+        rounded
+        small
+        @click="suma()"
+    >Sumar</v-btn>
+
+    <v-btn
+        color="primary"
+        elevation="7"
+        rounded
+        small
+        @click="resta()"
+    >Restar</v-btn>  
+    
+    <br />
     <span>{{ resultado }}</span>
 </div>
 </template>
@@ -16,11 +46,19 @@ export default {
             n1:null, 
             n2:null, 
             resultado:"",
+            rules: [
+                value => !!value || 'Requerido.',
+                value => (value && value.length >= 1) || 'Min 1 caracteres',
+                value => value === 'number' || 'No es un número',
+            ],
         };
     },
     methods: {
         suma() {
         this.resultado = parseFloat(this.n1) + parseFloat(this.n2);
+        },
+        resta() {
+        this.resultado = parseFloat(this.n1) - parseFloat(this.n2);
         },
     }, 
 };
